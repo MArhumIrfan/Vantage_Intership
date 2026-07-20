0b805e83e8e734d907e5eb844f5496885f2cf7fa
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-07-20 20:30:18 +0500

    Laerning C#

---
+++ Learning code/Numbers.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+           
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        Console.WriteLine("the sum of " + a + " and " + b + " is = " + sum);
+
+    }
+}
