712ef3e5654f85f1c68037e758159f420741a29b
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-07-28 13:31:33 +0500

    Pratice code

---
+++ Day-07/Practice code.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Application1
+{
+    
+class program{
+
+static void main (string []args)
+    {
+        
+        person myObj = new person();
+        myObj.name = "Arhum";
+        Console.WriteLine(myObj.name);
+
+
+    }
+
+
+}
+}
