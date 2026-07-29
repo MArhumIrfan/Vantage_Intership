87e96b3a11a2e6bd212260ed7cfd7f7b5cd11f98
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-07-29 13:51:49 +0500

    uploading

---
+++ Week-02/Day-08/pratice-code.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Pratice
+{
+    
+    public class vehicle
+    {
+        private string brand ="Ford";
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public void Horn()
+        {
+            Console.WriteLine("Beep!");
+        }
+
+    }
+
+    class Car: Vehicle
+    {
+        
+        public string modelName = "Mustang BOSS 429";
+        private int modelYear = 1969;
+        public string ModelYear
+        {
+            get{return modelYear;}
+            set{modelYear = date;}
+        }
+
+    }
+
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+           vehicle Vehicle = new vehicle();
+
+            vehicle.honk();
+            vehicle.honk();
+            Console.WriteLine("The car is : "+Vehicle.Brand()+" and the model is :"+Vehicle.modelName()+" which was produced in :"+Vehicle.modelYear());
+
+            
+        }
+    }
+}
