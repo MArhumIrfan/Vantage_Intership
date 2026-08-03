f373bb8a424049ece383f5a23c9f0c4dcdc47fac
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-08-03 20:45:05 +0500

    changes

--- Week-03/Day-11/Library-manage-system.cs
+++ Week-03/Day-11/Library-manage-system.cs
@@ -941,7 +941,7 @@ namespace Lib
 
                 DisplayRandomQuote();
 
-                Console.WriteLine("Identify your role (Admin/User/Guest) or type 'exit' to quit: ");
+                Console.WriteLine("Identify your role (Admin/User/Guest) or type -'exit' to quit: ");
                 string inputRole = Console.ReadLine() ?? "";
 
                 if (inputRole.Equals("exit", StringComparison.OrdinalIgnoreCase))
