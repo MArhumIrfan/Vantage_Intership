534e0246cdf1a41235d8efa6bef9c7422208acfe
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-08-11 18:35:32 +0500

    Changes

--- Week-04/Day-17/MyMinimalApi/Program.cs
+++ Week-04/Day-17/MyMinimalApi/Program.cs
@@ -8,3 +8,31 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summeries = new []
+{
+    "Freezing","Bracing","Chilly","Cool","Mild","Warm","Balmy","Hot","Swelting","Scoraching"
+};
+
+app.MapGet("/weather", ()=>
+{
+    var forecast = Enumerable.Range(1,5).Select(index =>
+    new WeatherForcast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20,55),
+            summeries[Random.Shared.Next(summeries.Length)] 
+        )
+    )
+    .toArray;
+    return forecast;    
+}
+
+)
+.WithName("GetWeatherForecast");
