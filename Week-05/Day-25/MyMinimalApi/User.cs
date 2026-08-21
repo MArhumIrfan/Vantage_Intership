87a91645ea5012559ce6af3467c63ad836def453
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-08-21 15:53:34 +0500

    Changes

--- Week-05/Day-25/MyMinimalApi/User.cs
+++ Week-05/Day-25/MyMinimalApi/User.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Lib
 {
     public class User
     {
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int UserId {get;set;}
         public string Name {get;set;} = string.Empty;
         public string Role {get;set;} = "User";
