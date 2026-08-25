d0de1756acb48fe8dd05015707377e0ebc988041
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-08-25 16:52:14 +0500

    Changes

--- Week-06/Day-27/Library_Management_System/Program.cs
+++ Week-06/Day-27/Library_Management_System/Program.cs
@@ -11,6 +11,7 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
 {
     options.SerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
+
 // Add Swagger services for testing
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
