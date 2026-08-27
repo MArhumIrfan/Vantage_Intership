ad6338aab1157ee192b498f03115bcd11bf0f700
MArhumIrfan <m.arhum.irfan@gmail.com>
2026-08-27 16:40:33 +0500

    Changes

--- Week-06/Day-29/Library_Management_System/Category.cs
+++ Week-06/Day-29/Library_Management_System/Category.cs
@@ -7,6 +7,11 @@ namespace Lib
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         
+        // Self-referencing relationship for subcategories (hierarchical stacking)
+        public int? ParentCategoryId { get; set; }
+        public Category? ParentCategory { get; set; }
+        public List<Category> SubCategories { get; set; } = new();
+
         // Navigation property for 1-to-many relationship (One category has many books)
         public List<Book> Books { get; set; } = new();
     }
